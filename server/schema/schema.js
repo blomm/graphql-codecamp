e34fc797fac15499e2ee3faac7149fd576b6ddee
Mike Blom <mikeblom@Mikes-MacBook-Pro.local>
2020-05-02 20:08:17 +0100

    query for joins

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,18 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 var books = [
-  { name: 'blah', genre: 'Fantasy', id: '1' },
-  { name: 'blah blah', genre: 'Fantasy', id: '2' },
-  { name: 'blah blah blah', genre: 'Sci-Fi', id: '3' },
+  { name: 'blah', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'blah blah', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'blah blah blah', genre: 'Sci-Fi', id: '3', authorId: '3' },
+];
+
+var authors = [
+  { name: 'dave', age: 19, id: '1' },
+  { name: 'claire', age: 13, id: '2' },
+  { name: 'tim', age: 41, id: '3' },
 ];
 
 const BookType = new GraphQLObjectType({
@@ -15,6 +21,21 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return authors.find((a) => a.id === parent.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    age: { type: GraphQLInt },
+    name: { type: GraphQLString },
   }),
 });
 
@@ -28,6 +49,15 @@ const RootQuery = new GraphQLObjectType({
         // code to get data from db / source
 
         return books.find((b) => b.id === args.id);
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / source
+
+        return authors.find((a) => a.id === args.id);
       },
     },
   },
